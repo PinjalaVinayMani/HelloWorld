9dcd27791a9cd8d46832f0447f4bcf2e058bbbce
PinjalaVinayMani <vinay2008pinjala@gmail.com>
2026-03-26 10:59:18 +0530

    Uc3 added

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
+        String name = "World";
         if (args.length > 0) {
-            for (String name : args) {
-                System.out.println("Hello, " + name + "!");
-            }
-        } else {
-            System.out.println("Hello, World!");
+            name = args[0]; 
         }
+        System.out.println("Hello, " + name + "!");
     }
 }
