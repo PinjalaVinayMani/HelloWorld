fbce84c3a98f3e09d4e403250b0aaa0a5feb2c4c
PinjalaVinayMani <vinay2008pinjala@gmail.com>
2026-04-13 10:08:16 +0530

    Uc7 added

--- HelloApp.java
+++ HelloApp.java
@@ -1,25 +1,10 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length > 0) {
-
-            for (String name : args) {
-                System.out.println("Hello, " + name + "!");
-            }
-
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-
-            for (String name : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(name);
-                first = false;
-            }
-            System.out.println("Hello, " + nameBuilder.toString() + "!");
-
-        } else {
+        if (args.length == 0) {
             System.out.println("Hello, World!");
+        } else {
+            String joinedNames = String.join(", ", args);
+            System.out.println("Hello, " + joinedNames + "!");
         }
     }
 }
